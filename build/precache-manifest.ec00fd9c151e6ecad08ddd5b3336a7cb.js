968804e9172fef386e0b905daee33e967f46ae13
FthRobby <rbysalam10@gmail.com>
2022-07-03 01:56:00 +0700

    bissmillah push file

---
+++ build/precache-manifest.ec00fd9c151e6ecad08ddd5b3336a7cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2da394fec1aa39ebe664eb2d38c4c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ad2237819bb4237acf",
+    "url": "/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "a4bf6a82c50c696c40bf",
+    "url": "/static/css/main.9d0cbd90.chunk.css"
+  },
+  {
+    "revision": "c5ad2237819bb4237acf",
+    "url": "/static/js/2.e2242ebd.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.e2242ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bf6a82c50c696c40bf",
+    "url": "/static/js/main.0d0ff11c.chunk.js"
+  },
+  {
+    "revision": "ba5fe43932f68fcc4eaa",
+    "url": "/static/js/runtime-main.4a59cdfd.js"
+  }
+]);
