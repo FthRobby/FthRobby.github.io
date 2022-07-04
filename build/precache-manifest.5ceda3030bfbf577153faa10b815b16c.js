0e92fa4a21c00e698109792dc44ab60ae4bd642b
FthRobby <rbysalam10@gmail.com>
2022-07-04 23:37:36 +0700

    add build folder

---
+++ build/precache-manifest.5ceda3030bfbf577153faa10b815b16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74c9aff13e22c2bf8295204ff54bbc9",
+    "url": "/fthrobby.github.io/index.html"
+  },
+  {
+    "revision": "c5ad2237819bb4237acf",
+    "url": "/fthrobby.github.io/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "ef558baaef8bcc10815b",
+    "url": "/fthrobby.github.io/static/css/main.7b124661.chunk.css"
+  },
+  {
+    "revision": "c5ad2237819bb4237acf",
+    "url": "/fthrobby.github.io/static/js/2.e2242ebd.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/fthrobby.github.io/static/js/2.e2242ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef558baaef8bcc10815b",
+    "url": "/fthrobby.github.io/static/js/main.0825fc59.chunk.js"
+  },
+  {
+    "revision": "ec2b86e7fdd9fb133364",
+    "url": "/fthrobby.github.io/static/js/runtime-main.7e85e64d.js"
+  }
+]);
